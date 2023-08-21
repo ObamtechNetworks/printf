e7722e9c166af2e25e087907a3b16ad0f9a31868
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-21 22:29:15 +0000

    Fix compiler errors

--- get_fmt_handler.c
+++ get_fmt_handler.c
@@ -4,16 +4,13 @@
  * @str: the char specifier to detect its matching function
  * Return: returns the length of the argunments printed
  */
-int (*get_fmt_handler(char str))(va_list)
+int (*get_fmt_handler(char *str))(va_list)
 {
 	check_format specifier_arr[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_integer},
-		{'i', print_integer}
-		/*{'n', print_newline},*/
-		/*{'t', print_tab}*/
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
 	};
 	int i = 0;
 	int size = sizeof(specifier_arr) / sizeof(specifier_arr[0]);
@@ -21,7 +18,7 @@ int (*get_fmt_handler(char str))(va_list)
 	while (i < size)
 	{
 		/*check comparsion of given argument if exists in arr struct*/
-		if (specifier_arr[i].specifier == str)
+		if (specifier_arr[i].specifier[0] == *str)
 		{
 			/* call or return the equivalent function pointer*/
 			return (specifier_arr[i].format_handler);
