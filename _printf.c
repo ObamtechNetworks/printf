d78e52ec4b1dc8d5f1b8fd16da31837677ba2b7b
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-21 20:59:15 +0000

    Implement printf functionality with limited lines of code

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _printf - (Produces output according to a format)
+ * @format: The format string
+ * Return: Number of bytes
+ */
+
+int _printf(const char *format, ...)
+{
+	int len = 0; /* declare and init len to keep track of bytes output */
+	va_list ab; /*create variable of type va_list */
+	char *charac; /* Pointer var to keep track of each char in format */
+
+	va_start(ab, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (charac = (char *)format; *charac; charac++)
+	{
+		if (*charac != '%')
+		{
+			len += write(1, &*charac, 1);
+			continue;
+		}
+		charac++;
+
+		if (get_spec_fun(charac))
+			len += get_func(charac, ab);
+		else
+			len += _putchar(*charac);
+	}
+	va_end(ab);
+	return (len);
+}
